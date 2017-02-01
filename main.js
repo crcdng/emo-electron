52c90c2e12d706bb495265f0c270fd5982225ccc
mike <straeubig@i3games.de>
2017-02-01 13:42:48 +0000

    resize window

--- main.js
+++ main.js
@@ -10,13 +10,13 @@ let mainWindow, previewWindow;
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow = new BrowserWindow({width: 800, height: 720});
 
   // and load the index.html of the app.
   mainWindow.loadURL(`file://${__dirname}/index.html`);
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools({mode: "undocked"});
+  // mainWindow.webContents.openDevTools({mode: "undocked"});
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
@@ -24,7 +24,7 @@ function createWindow () {
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
     mainWindow = null;
-  })  
+  })
 }
 
 // This method will be called when Electron has finished
